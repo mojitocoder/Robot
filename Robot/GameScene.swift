226ef746f20a12b8b6b0f1d7b201b51e0c72ac85
Quynh Nguyen <qn@rosslynanalytics.com>
2015-09-02 20:52:12 +0100

    Black holes move randomly

--- Robot/GameScene.swift
+++ Robot/GameScene.swift
@@ -74,20 +74,70 @@ class GameScene: SKScene {
         //let moveAction = SKAction.repeatActionForever(actionSequence)
         
         for i in 1...10 {
-            
+
             var blackHoleNode = SKSpriteNode(imageNamed: "BlackHole0")
-            blackHoleNode.position = CGPointMake(100 + CGFloat(i) * 150, 50)
+            
+            let movementTimeInSecs: Double = 5.0
+            
+            //put the black hole in a random y position
+            // then work out a random movement speed for it
+            let lowerYBound: Int = Int(blackHoleNode.size.height / 2)
+            let upperYBound: Int = Int(self.size.height - blackHoleNode.size.height / 2)
+            var randomYPos = randomInt(lowerYBound, max: upperYBound)
+            
+            var moveAction: SKAction
+            
+            //generate a random moving direction
+            var moveUp: Bool = randomInt(1, max: 10) % 2 == Int(0)
+            if (moveUp) {
+                //calculate the proportional time to travel the rest of the distance
+                var timeTaken = (Double(upperYBound - randomYPos) / Double(upperYBound - lowerYBound)) * movementTimeInSecs;
+                
+                let moveUp: SKAction = SKAction.moveToY(CGFloat(upperYBound), duration: NSTimeInterval(timeTaken))
+                let moveDown: SKAction = SKAction.moveToY(CGFloat(lowerYBound), duration: movementTimeInSecs)
+                let moveUpAgain: SKAction = SKAction.moveToY(CGFloat(upperYBound), duration: movementTimeInSecs)
+                let moveUpDown: SKAction = SKAction.repeatActionForever(SKAction.sequence([moveDown, moveUpAgain]))
+                moveAction = SKAction.sequence([moveUp, moveUpDown])
+            }
+            else {
+                //calculate the proportional time to travel the rest of the distance
+                var timeTaken = (Double(randomYPos - lowerYBound) / Double(upperYBound - lowerYBound)) * movementTimeInSecs;
+                
+                let moveDown: SKAction = SKAction.moveToY(CGFloat(lowerYBound), duration: NSTimeInterval(timeTaken))
+                let moveUp: SKAction = SKAction.moveToY(CGFloat(upperYBound), duration: movementTimeInSecs)
+                let moveDownAgain: SKAction = SKAction.moveToY(CGFloat(lowerYBound), duration: movementTimeInSecs)
+                let moveUpDown: SKAction = SKAction.repeatActionForever(SKAction.sequence([moveUp, moveDownAgain]))
+                moveAction = SKAction.sequence([moveDown, moveUpDown])
+            }
+            
+            blackHoleNode.position = CGPointMake(100 + CGFloat(i) * 100, CGFloat(randomYPos))
+            
             //blackHoleNode.physicsBody = SKPhysicsBody(circleOfRadius: blackHoleNode.size.width / 2)
             //blackHoleNode.physicsBody!.dynamic = false
             //blackHoleNode.physicsBody!.categoryBitMask = CollisionCategoryBlackHoles
             //blackHoleNode.physicsBody!.collisionBitMask = 0
             //blackHoleNode.name = BlackHoleName
             
-            //blackHoleNode.runAction(moveAction)
+            blackHoleNode.runAction(moveAction)
             blackHoleNode.runAction(rotateAction)
             
             self._foreground!.addChild(blackHoleNode)
         }
+        
+        
+        
+        for i in 1...20 {
+            var k: Int = randomInt(1, max: 10)
+            var moveUp: Bool = k % 2 == Int(0)
 
+            println("Random number = \(k), odd = \(moveUp)")
+        }
+    }
+    
+    /* Function to generate a random integer between two inclusive bounds
+        http://stackoverflow.com/questions/24007129/how-does-one-generate-a-random-number-in-apples-swift-language
+    */
+    func randomInt(min: Int, max:Int) -> Int {
+        return min + Int(arc4random_uniform(UInt32(max - min + 1)))
     }
 }
